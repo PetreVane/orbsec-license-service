2dc31f536b758d6f7a9732b53bfb869cb17cb023
Petre Vane <petre.vane@gmail.com>
2021-11-18 15:43:41 +0100

    Added LicenseServiceTest

--- src/test/java/com/orbsec/licensingservice/service/LicenseServiceTest.java
+++ src/test/java/com/orbsec/licensingservice/service/LicenseServiceTest.java
@@ -1,48 +1,88 @@
 package com.orbsec.licensingservice.service;
 
+import com.orbsec.licensingservice.exception.MissingLicenseException;
 import com.orbsec.licensingservice.model.License;
 import com.orbsec.licensingservice.repository.LicenseRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.then;
+
+import org.mockito.*;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 import java.util.Optional;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.error.ShouldBeEmpty.shouldBeEmpty;
+import static org.mockito.Mockito.*;
 
 @DataJpaTest
 class LicenseServiceTest {
 
-    @Autowired
-    private LicenseRepository underTest;
+    @Captor
+    ArgumentCaptor<License> licenseArgumentCaptor;
 
+    private LicenseRepository licenseRepository = mock(LicenseRepository.class);
+
+    @InjectMocks
+    private LicenseService underTest;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void itShouldCreateLicense() {
         // Given
-        License testLicense = new License("0000", "Under test license", "Under test Organization", "Under test product", "Under test license type");
+        String organizationId = "Under test Organization";
+        String licenseId = "0001";
+        License testLicense = new License(licenseId, "Under test license", organizationId, "Under test product", "Under test license type");
+
         // When
-        underTest.save(testLicense);
+        underTest.createLicense(testLicense, organizationId);
         // Then
-        Optional<License> fetchedLicense = underTest.findLicenseByLicenseId(testLicense.getLicenseId());
-
-        assertThat(fetchedLicense).get()
-                .isEqualTo(testLicense);
-
+        then(licenseRepository).should().save(licenseArgumentCaptor.capture());
+        License fetchedLicense = licenseArgumentCaptor.getValue();
+        assertThat(fetchedLicense).isEqualTo(testLicense).isInstanceOf(License.class);
     }
 
 
     @Test
+    void itShouldThrowException() {
+        // Given
+        String organizationId = "Under test Organization";
+        String licenseId = "0001";
+        // Then
+        assertThatThrownBy(() -> underTest.deleteLicense(licenseId, organizationId)).isInstanceOf(MissingLicenseException.class);
+    }
+
+    @Test
     void itShouldDeleteLicense() {
         // Given
-        License testLicense = new License("0000", "Under test license", "Under test Organization", "Under test product", "Under test license type");
+        String organizationId = "Under test Organization";
+        String licenseId = "0001";
+        License testLicense = new License(licenseId, "Under test license", organizationId, "Under test product", "Under test license type");
+
         // When
-        underTest.save(testLicense);
-        License toBeDeletedLicense = underTest.findLicenseByLicenseId(testLicense.getLicenseId()).get();
+        when(licenseRepository.findLicenseByLicenseId(licenseId)).thenReturn(Optional.of(testLicense));
+        underTest.deleteLicense(licenseId, organizationId);
         // Then
-         underTest.delete(toBeDeletedLicense);
-        Optional<License> emptyOptional = underTest.findLicenseByLicenseId(testLicense.getLicenseId());
-        shouldBeEmpty(emptyOptional);
+        verify(licenseRepository).delete(testLicense);
+    }
+
+    @Test
+    void itShouldUpdateLicense() {
+        // Given
+        String organizationId = "Under test Organization";
+        String licenseId = "0001";
+        License testLicense = new License(licenseId, "Under test license", organizationId, "Under test product", "Under test license type");
+
+        // When
+        when(licenseRepository.findLicenseByLicenseId(licenseId)).thenReturn(Optional.of(testLicense));
+        // Then
+        underTest.updateLicense(testLicense, organizationId);
+        verify(licenseRepository).save(testLicense);
+        verify(licenseRepository).findLicenseByLicenseId(licenseId);
     }
 }
+
