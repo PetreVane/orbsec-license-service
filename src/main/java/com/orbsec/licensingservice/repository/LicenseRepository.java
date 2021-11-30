ca2678e2b59d73761dcd43dd8bed14aceddefdaa
Petre Vane <petre.vane@gmail.com>
2021-11-30 12:47:31 +0100

    Added new crud methods

--- src/main/java/com/orbsec/licensingservice/repository/LicenseRepository.java
+++ src/main/java/com/orbsec/licensingservice/repository/LicenseRepository.java
@@ -2,10 +2,18 @@ package com.orbsec.licensingservice.repository;
 
 import com.orbsec.licensingservice.model.License;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-public interface LicenseRepository extends CrudRepository<License, UUID> {
+@Repository
+public interface LicenseRepository extends CrudRepository<License, String> {
     Optional<License> findLicenseByLicenseId(String licenseId);
+
+    List<License> findLicenseByOrganizationId(String organizationId);
+
+    Optional<License> findLicenseByOrganizationIdAndLicenseId(String organizationId, String licenseId);
+
+
 }
