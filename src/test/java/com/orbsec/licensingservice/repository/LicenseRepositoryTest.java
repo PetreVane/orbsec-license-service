1129a393caf27b999893420a2117d2200d4d0569
Petre Vane <petre.vane@gmail.com>
2022-03-12 16:37:16 +0100

    Changes: - added new test for RedisService - improved test coverage for LicenseRepository

--- src/test/java/com/orbsec/licensingservice/repository/LicenseRepositoryTest.java
+++ src/test/java/com/orbsec/licensingservice/repository/LicenseRepositoryTest.java
@@ -2,14 +2,23 @@ package com.orbsec.licensingservice.repository;
 
 import com.orbsec.licensingservice.model.License;
 
+import java.util.Collection;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Description;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest(properties = {"spring.jpa.properties.javax.persistence.validation.mode=none"})
 class LicenseRepositoryTest {
@@ -17,42 +26,65 @@ class LicenseRepositoryTest {
     @Autowired
     private LicenseRepository licenseRepository;
 
-    @Autowired
-    private LicenseRepository underTest;
-
     @Test
+    @Description("It should find License by id")
     void itShouldFindLicenseByLicenseId() {
-        // Given
-        String testLicenseId = "testLicense id";
-        License testLicense = new License(testLicenseId, "Test description", "Test Corp", "Test Product", "Test type");
-        // When
-        underTest.save(testLicense);
+        License license = new License();
+        license.setComment("Comment");
+        license.setContactEmail("jane.doe@example.org");
+        license.setContactName("Contact Name");
+        license.setContactPhone("4105551212");
+        license.setDescription("Description");
+        license.setLicenseId("12342");
+        license.setLicenseType("License Type");
+        license.setOrganizationId("12342");
+        license.setOrganizationName("Organization Name");
+        license.setProductName("Product Name");
 
-        // Then
-        assertThat(underTest.findLicenseByLicenseId("Incorrect id")).isNotPresent();
-        assertThat(underTest.findLicenseByLicenseId(testLicenseId).isPresent());
-        assertThat(underTest.findLicenseByLicenseId(testLicenseId).get()).isInstanceOf(License.class);
-        assertThat(underTest.findLicenseByLicenseId(testLicenseId).get().getLicenseId().equals(testLicenseId));
+        this.licenseRepository.save(license);
+        assertTrue(this.licenseRepository.findLicenseByLicenseId("12342").isPresent());
     }
 
+
     @Test
+    @Description("It should find License by Organization id")
     void itShouldFindAllByOrganizationId() {
         License license = new License();
         license.setComment("Comment");
         license.setContactEmail("jane.doe@example.org");
         license.setContactName("Contact Name");
         license.setContactPhone("4105551212");
-        license.setDescription("The characteristics of someone or something");
-        license.setLicenseId("42");
+        license.setDescription("Description");
+        license.setLicenseId("12342");
         license.setLicenseType("License Type");
-        license.setOrganizationId("42");
+        license.setOrganizationId("12342");
         license.setOrganizationName("Organization Name");
         license.setProductName("Product Name");
 
         this.licenseRepository.save(license);
-        String string = "foo";
 
-        List<License> actualFindAllByOrganizationIdResult = this.licenseRepository.findAllByOrganizationId(string);
-        assertTrue(actualFindAllByOrganizationIdResult.isEmpty());
+        List<License> actualResult = this.licenseRepository.findAllByOrganizationId("12342");
+        assertTrue(actualResult.contains(license));
+    }
+
+
+    @Test
+    @Description("It should find all Licenses")
+    void testFindAll() {
+        License license = new License();
+        license.setComment("Comment");
+        license.setContactEmail("jane.doe@example.org");
+        license.setContactName("Contact Name");
+        license.setContactPhone("4105551212");
+        license.setDescription("Description");
+        license.setLicenseId("12342");
+        license.setLicenseType("License Type");
+        license.setOrganizationId("12342");
+        license.setOrganizationName("Organization Name");
+        license.setProductName("Product Name");
+
+        this.licenseRepository.save(license);
+        assertEquals(1, ((Collection<License>) this.licenseRepository.findAll()).size());
+        assertTrue(((Collection<License>) this.licenseRepository.findAll()).contains(license));
     }
 }
---
+++ src/test/java/com/orbsec/licensingservice/service/RedisServiceTest.java
@@ -0,0 +1,103 @@
+package com.orbsec.licensingservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.orbsec.licensingservice.model.OrganizationDto;
+import com.orbsec.licensingservice.repository.OrganizationRedisRepository;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Description;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ContextConfiguration(classes = {RedisService.class})
+@ExtendWith(SpringExtension.class)
+class RedisServiceTest {
+    @MockBean
+    private OrganizationRedisRepository organizationRedisRepository;
+
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    @Description("It should save Organization record into Redis database")
+    void itShouldSaveRecordIntoCache() {
+        OrganizationDto organizationDto = new OrganizationDto();
+        organizationDto.setContactEmail("jane.doe@example.org");
+        organizationDto.setContactName("Contact Name");
+        organizationDto.setContactPhone("4105551212");
+        organizationDto.setId("12342");
+        organizationDto.setName("Name");
+
+        when(this.organizationRedisRepository.save(any())).thenReturn(organizationDto);
+
+        this.redisService.saveRecordIntoCache(organizationDto);
+        verify(this.organizationRedisRepository).save(any());
+
+        assertEquals("jane.doe@example.org", organizationDto.getContactEmail());
+        assertEquals("Name", organizationDto.getName());
+        assertEquals("12342", organizationDto.getId());
+        assertEquals("4105551212", organizationDto.getContactPhone());
+        assertEquals("Contact Name", organizationDto.getContactName());
+    }
+
+    @Test
+    @Description("It should check cache for Organization record")
+    void itShouldCheckCacheFor() {
+
+        OrganizationDto organizationDto = new OrganizationDto();
+        organizationDto.setContactEmail("jane.doe@example.org");
+        organizationDto.setContactName("Contact Name");
+        organizationDto.setContactPhone("4105551212");
+        organizationDto.setId("12342");
+        organizationDto.setName("Name");
+        Optional<OrganizationDto> optionalResult = Optional.of(organizationDto);
+
+        when(this.organizationRedisRepository.findById(any())).thenReturn(optionalResult);
+        Optional<OrganizationDto> actualResult = this.redisService.checkCacheFor("12342");
+        assertSame(optionalResult, actualResult);
+        assertTrue(actualResult.isPresent());
+        verify(this.organizationRedisRepository).findById(any());
+    }
+
+    @Test
+    @Description("It should delete cache for Organization record")
+    void itShouldEvictCacheFor() {
+
+        OrganizationDto organizationDto = new OrganizationDto();
+        organizationDto.setContactEmail("jane.doe@example.org");
+        organizationDto.setContactName("Contact Name");
+        organizationDto.setContactPhone("4105551212");
+        organizationDto.setId("12342");
+        organizationDto.setName("Name");
+
+        Optional<OrganizationDto> optionalResult = Optional.of(organizationDto);
+        doNothing().when(this.organizationRedisRepository).delete(any());
+
+        when(this.organizationRedisRepository.findById(any())).thenReturn(optionalResult);
+        this.redisService.evictCacheFor("12342");
+        verify(this.organizationRedisRepository).findById(any());
+        verify(this.organizationRedisRepository).delete(any());
+    }
+
+    @Test
+    @Description("It should not delete cache for Organization record")
+    void itShouldNotEvictCacheFor() {
+        doNothing().when(this.organizationRedisRepository).delete(any());
+        when(this.organizationRedisRepository.findById(any())).thenReturn(Optional.empty());
+        this.redisService.evictCacheFor("12342");
+        verify(this.organizationRedisRepository).findById(any());
+    }
+}
+
