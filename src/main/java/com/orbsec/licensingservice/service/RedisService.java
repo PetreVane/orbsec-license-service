2ec5837fe6beb422af21349ba63aa9e84477b688
Petre Vane <petre.vane@gmail.com>
2022-03-12 13:38:40 +0100

    Changes: - added circuit breaker fallback logic for RedisService
    Signed-off-by: Petre Vane <petre.vane@gmail.com>

--- src/main/java/com/orbsec/licensingservice/service/RedisService.java
+++ src/main/java/com/orbsec/licensingservice/service/RedisService.java
@@ -1,8 +1,10 @@
 package com.orbsec.licensingservice.service;
 
-import com.orbsec.licensingservice.exception.RedisConnectionException;
 import com.orbsec.licensingservice.model.OrganizationDto;
 import com.orbsec.licensingservice.repository.OrganizationRedisRepository;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,37 +16,38 @@ import java.util.Optional;
 public class RedisService {
 
     private final OrganizationRedisRepository redisRepository;
+    private static final String NOT_AVAILABLE = "Data not available";
+    private static final String DESCRIPTION = "Redis database service is not available. Try again later!";
+    private static final String EXCEPTION_MESSAGE = "Exception message: {}";
 
     @Autowired
     public RedisService(OrganizationRedisRepository redisRepository) {
         this.redisRepository = redisRepository;
     }
 
-
+    @CircuitBreaker(name = "redisDatabase", fallbackMethod = "saveRecordIntoCacheFallback")
+    @Retry(name ="retryRedisDatabase", fallbackMethod = "saveRecordIntoCacheFallback")
+    @Bulkhead(name = "bulkheadRedisDatabase", fallbackMethod = "saveRecordIntoCacheFallback")
     public void saveRecordIntoCache(OrganizationDto organization) {
         log.info("Attempting to save organization record into Redis cache. Id: {}", organization.getId());
-        try {
-            redisRepository.save(organization);
-            log.info("Successfully cached organization record with id {}", organization.getId());
-        } catch (Exception e) {
-            log.error("Errors while attempting to cache organization record: {}", e.getMessage());
-        }
+        redisRepository.save(organization);
+        log.info("Successfully cached organization record with id {}", organization.getId());
     }
 
-    // checks redis cache for record
+    @CircuitBreaker(name = "redisDatabase", fallbackMethod = "checkCacheFallback")
+    @Retry(name ="retryRedisDatabase", fallbackMethod = "checkCacheFallback")
+    @Bulkhead(name = "bulkheadRedisDatabase", fallbackMethod = "checkCacheFallback")
     public Optional<OrganizationDto> checkCacheFor(String organizationId) {
         log.info("Attempting to get organization record from Redis cache, for id {}", organizationId);
-        try {
-            var cachedRecord =  redisRepository.findById(organizationId);
-            log.info("Successfully fetched Redis cached record with id {}", organizationId);
-            return cachedRecord;
-        } catch (Exception e) {
-            log.error("Errors while trying to get record from Redis cache: {}", e.getMessage());
-            throw new RedisConnectionException(e.getMessage());
-        }
+        var cachedRecord =  redisRepository.findById(organizationId);
+        log.info("Successfully fetched Redis cached record with id {}", organizationId);
+        return cachedRecord;
     }
 
-    // removes a record from redis cache
+
+    @CircuitBreaker(name = "redisDatabase", fallbackMethod = "evictCacheFallback")
+    @Retry(name ="retryRedisDatabase", fallbackMethod = "evictCacheFallback")
+    @Bulkhead(name = "bulkheadRedisDatabase", fallbackMethod = "evictCacheFallback")
     public void evictCacheFor(String organizationId) {
         log.info("Attempting to evict redis record for id: {}", organizationId);
         var cachedRecord = checkCacheFor(organizationId);
@@ -54,5 +57,34 @@ public class RedisService {
         } else {
             log.info("No cached record was found for id: {}", organizationId);
         }
+    }
+
+    // Fallbacks
+    @SuppressWarnings("unused")
+    private void saveRecordIntoCacheFallback(OrganizationDto organizationDto, Throwable exception) {
+        log.error("@CircuitBreaker: saveRecordIntoCacheFallback called for Organization id: {}", organizationDto.getId());
+        log.error(EXCEPTION_MESSAGE,  exception.getMessage());
+    }
+
+    @SuppressWarnings("unused")
+    private Optional<OrganizationDto> checkCacheFallback(String organizationId, Throwable exception) {
+        log.error("@CircuitBreaker: checkCacheFallback called for Organization id: {}", organizationId);
+        log.error(EXCEPTION_MESSAGE, exception.getMessage());
+        return Optional.of(generateFallbackObject());
+    }
+
+    @SuppressWarnings("unused")
+    private void evictCacheFallback(String organizationId, Throwable exception) {
+        log.error("@CircuitBreaker: evictCacheFallback called for Organization id: {}", organizationId);
+        log.error(EXCEPTION_MESSAGE, exception.getMessage());
+    }
+
+    private OrganizationDto generateFallbackObject() {
+        var fallbackObject = new OrganizationDto();
+        fallbackObject.setName(DESCRIPTION);
+        fallbackObject.setContactEmail(NOT_AVAILABLE);
+        fallbackObject.setContactName(NOT_AVAILABLE);
+        fallbackObject.setContactPhone(NOT_AVAILABLE);
+        return fallbackObject;
     }
 }
